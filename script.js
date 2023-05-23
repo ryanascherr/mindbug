d7ec246d553bec21c19c33cb17a02f567d366148
ryanascherr <ryanascherr@gmail.com>
2023-05-23 16:49:17 -0400

    Added images. Created array of objects for creatures.

---
+++ script.js
@@ -0,0 +1,266 @@
+let creatures = [
+    {
+        name: "Axolotl Healer",
+        keywords: "Poisonous",
+        triggers: "Play",
+        pack: "First Contact"
+    },
+    {
+        name: "Bee Bear",
+        keywords: "",
+        triggers: "",
+        pack: "First Contact"
+    },
+    {
+        name: "Brain Fly",
+        keywords: "",
+        triggers: "Play",
+        pack: "First Contact"
+    },
+    {
+        name: "Chameleon Sniper",
+        keywords: "Sneaky",
+        triggers: "Attack",
+        pack: "First Contact"
+    },
+    {
+        name: "Compost Dragon",
+        keywords: "Hunter",
+        triggers: "Play",
+        pack: "First Contact"
+    },
+    {
+        name: "Deathweaver",
+        keywords: "Poisonous",
+        triggers: "",
+        pack: "First Contact"
+    },
+    {
+        name: "Elephantopus",
+        keywords: "Tough",
+        triggers: "",
+        pack: "First Contact"
+    },
+    {
+        name: "Explosive Toad",
+        keywords: "Frenzy",
+        triggers: "Defeated",
+        pack: "First Contact"
+    },
+    {
+        name: "Ferret Bomber",
+        keywords: "Sneaky",
+        triggers: "Play",
+        pack: "First Contact"
+    },
+    {
+        name: "Giraffodile",
+        keywords: "",
+        triggers: "Play",
+        pack: "First Contact"
+    },
+    {
+        name: "Goblin Werewolf",
+        keywords: "Hunter",
+        triggers: "",
+        pack: "First Contact"
+    },
+    {
+        name: "Gorillion",
+        keywords: "",
+        triggers: "",
+        pack: "First Contact"
+    },
+    {
+        name: "Grave Robber",
+        keywords: "Tough",
+        triggers: "Play",
+        pack: "First Contact"
+    },
+    {
+        name: "Harpy Mother",
+        keywords: "",
+        triggers: "Defeated",
+        pack: "First Contact"
+    },
+    {
+        name: "Kangasaurus Rex",
+        keywords: "",
+        triggers: "Play",
+        pack: "First Contact"
+    },
+    {
+        name: "Killer Bee",
+        keywords: "Hunter",
+        triggers: "Play",
+        pack: "First Contact"
+    },
+    {
+        name: "Lone Yeti",
+        keywords: "Tough, Frenzy",
+        triggers: "",
+        pack: "First Contact"
+    },
+    {
+        name: "Luchataur",
+        keywords: "Frenzy",
+        triggers: "",
+        pack: "First Contact"
+    },
+    {
+        name: "Mysterious Mermaid",
+        keywords: "",
+        triggers: "Play",
+        pack: "First Contact"
+    },
+    {
+        name: "Plated Scorpion",
+        keywords: "Tough, Poisonous",
+        triggers: "",
+        pack: "First Contact"
+    },
+    {
+        name: "Rhino Turtle",
+        keywords: "Frenzy, Tough",
+        triggers: "",
+        pack: "First Contact"
+    },
+    {
+        name: "Shark Dog",
+        keywords: "Hunter",
+        triggers: "Attack",
+        pack: "First Contact"
+    },
+    {
+        name: "Sharky Crab-Dog-Mummypus",
+        keywords: "Hunter, Sneaky, Frenzy, Poisonous",
+        triggers: "",
+        pack: "First Contact"
+    },
+    {
+        name: "Shield Bugs",
+        keywords: "Tough",
+        triggers: "",
+        pack: "First Contact"
+    },
+    {
+        name: "Snail Hydra",
+        keywords: "",
+        triggers: "Attack",
+        pack: "First Contact"
+    },
+    {
+        name: "Snail Thrower",
+        keywords: "Poisonous, Hunter",
+        triggers: "",
+        pack: "First Contact"
+    },
+    {
+        name: "Spider Owl",
+        keywords: "Sneaky, Poisonous",
+        triggers: "",
+        pack: "First Contact"
+    },
+    {
+        name: "Strange Barrel",
+        keywords: "Defeated",
+        triggers: "",
+        pack: "First Contact"
+    },
+    {
+        name: "Tiger Squirrel",
+        keywords: "Sneaky",
+        triggers: "Play",
+        pack: "First Contact"
+    },
+    {
+        name: "Turbo Bug",
+        keywords: "",
+        triggers: "Attack",
+        pack: "First Contact"
+    },
+    {
+        name: "Tusked Extorter",
+        keywords: "",
+        triggers: "Attack",
+        pack: "First Contact"
+    },
+    {
+        name: "Urchin Hunter",
+        keywords: "Hunter",
+        triggers: "",
+        pack: "First Contact"
+    },
+    {
+        name: "Bugzerker",
+        keywords: "Tough",
+        triggers: "",
+        pack: "First Contact: Add-On"
+    },
+    {
+        name: "Count Draculeech",
+        keywords: "",
+        triggers: "Attack",
+        pack: "First Contact: Add-On"
+    },
+    {
+        name: "Creep from the Deep",
+        keywords: "Poisonous, Hunter",
+        triggers: "",
+        pack: "First Contact: Add-On"
+    },
+    {
+        name: "Ferret Pacifier",
+        keywords: "",
+        triggers: "",
+        pack: "First Contact: Add-On"
+    },
+    {
+        name: "Froblin Instigator",
+        keywords: "Hunter",
+        triggers: "",
+        pack: "First Contact: Add-On"
+    },
+    {
+        name: "Goreagle Aplha",
+        keywords: "Frenzy, Hunter, Tough",
+        triggers: "Play",
+        pack: "First Contact: Add-On"
+    },
+    {
+        name: "Hamster Lion",
+        keywords: "Frenzy",
+        triggers: "",
+        pack: "First Contact: Add-On"
+    },
+    {
+        name: "Hungry Hungry Hamster",
+        keywords: "Sneaky",
+        triggers: "Play",
+        pack: "First Contact: Add-On"
+    },
+    {
+        name: "Hyenix",
+        keywords: "Frenzy",
+        triggers: "",
+        pack: "First Contact: Add-On"
+    },
+    {
+        name: "Majestic Manticore",
+        keywords: "Poisonous",
+        triggers: "Attack",
+        pack: "First Contact: Add-On"
+    },
+    {
+        name: "The Lurker",
+        keywords: "Tough, Sneaky",
+        triggers: "Attack",
+        pack: "First Contact: Add-On"
+    },
+    {
+        name: "Turf the Surfer",
+        keywords: "",
+        triggers: "Attack",
+        pack: "First Contact: Add-On"
+    }
+]
